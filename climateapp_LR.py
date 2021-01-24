2e9ea364df9dd01eb88619c6b57fdfe07a684e7f
thallegacy <thallegacy@users.noreply.github.com>
2021-01-24 09:57:06 -0500

    app database setup

---
+++ climateapp_LR.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
